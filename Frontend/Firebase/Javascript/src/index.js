7353db63bbc467fd127a8aba0105bb70c803aac1
NivNavick <nnavick@gmail.com>
2023-04-19 10:51:39 +0300

    init ownid app

--- Frontend/Firebase/Javascript/src/index.js
+++ Frontend/Firebase/Javascript/src/index.js
@@ -7,14 +7,14 @@ import {
     signOut
 } from "firebase/auth";
 import {initializeApp} from "firebase/app";
-  ((o,w,n,i,d)=>{o[i]=o[i]||(async(...a)=>((o[i].q=o[i].q||[]).push(a),{error:null,data:null})),
-  (d=w.createElement("script")).src='https://cdn.dev.ownid.com/sdk/'+n,d.async=1,w.head.appendChild(d)})
-  (window,document,'{app_id}','ownid');
-  // TODO: add your app id here from your OwnID console app (you can use your snippet) https://console.ownid.com/
 const firebaseConfig = {
     //Firebase config goes here
 };
 initializeApp(firebaseConfig);
+((o,w,n,i,d)=>{o[i]=o[i]||(async(...a)=>((o[i].q=o[i].q||[]).push(a),{error:null,data:null})),
+(d=w.createElement("script")).src='https://cdn.dev.ownid.com/sdk/'+n,d.async=1,w.head.appendChild(d)})
+(window,document,'{app_id}','ownid');
+  // TODO: add your app id here from your OwnID console app (you can use your snippet) https://console.ownid.com/
 ownid('init', {
     firebaseAuth: {getAuth, getIdToken, signInWithCustomToken},
 })
